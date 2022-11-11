0e6a8d6d4d682123cc5ff59bc6fa58f6b35b74d8
k0t3sla <lifa-ali@yandex.ru>
2022-11-11 15:27:05 +0300

    refactor

--- src/stack_stat/core.clj
+++ src/stack_stat/core.clj
@@ -31,55 +31,55 @@
 
 (defn map-pipeline-async
   [async-function p coll]
-   (let [in-c (a/chan p)
-         out-c (a/chan p)]
-     (a/onto-chan! in-c coll)
-     (a/pipeline-async p out-c async-function in-c)
-     (seq-of-chanels out-c)))
+  (let [in-c (a/chan p)
+        out-c (a/chan p)]
+    (a/onto-chan! in-c coll)
+    (a/pipeline-async p out-c async-function in-c)
+    (seq-of-chanels out-c)))
 
 (defn parse-tags [params]
   (let [tags (->> (form-decode params)
                   walk/keywordize-keys)]
-    (cond
-      (string? (:tag tags)) {:tag (:tag tags)}
-      (vector? (:tag tags)) (vec (for [tag (:tag tags)]
-                                   {:tag tag}))
-      :else nil)))
+    (if (string? (:tag tags))
+      [(:tag tags)]
+      (:tag tags))))
 
-(defn format-tag-data [page body]
+(defn format-tag-data [tag body]
   (let [parsed-data (walk/keywordize-keys (cheshire/parse-string body))]
-    {(:tag page) {:total (total (walk/keywordize-keys parsed-data))
-                  :answered (is-answered (walk/keywordize-keys parsed-data))}}))
+    {(keyword tag) {:total (total (walk/keywordize-keys parsed-data))
+                     :answered (is-answered (walk/keywordize-keys parsed-data))}}))
 
+(defn make-async-reqest [tag c]
+  (client/get
+   "https://api.stackexchange.com/2.2/search"
+   {:query-params {:pagesize 100
+                   :order "desc"
+                   :sort "creation"
+                   :tagged tag
+                   :site "stackoverflow"}}
+   (fn [{:keys [body]}]
+     (when (string? body)
+       (a/>!! c (format-tag-data tag body)))
+     (a/close! c))))
 
 (defn request-stackoverflow [tags treads]
-  (map-pipeline-async
-   (fn [{:keys [tag] :as page} c]
-     (client/get "https://api.stackexchange.com/2.2/search"
-                 {:query-params {:pagesize 100
-                                 :order "desc"
-                                 :sort "creation"
-                                 :tagged tag
-                                 :site "stackoverflow"}}
-                 (fn [{:keys [body]}]
-                   (when (string? body)
-                     (a/>!! c (format-tag-data page body)))
-                   (a/close! c))))
-   treads tags))
-  
+  (map-pipeline-async make-async-reqest treads tags))
+
 (defn app [req]
   (let [qs (:query-string req)
         tags (parse-tags qs)
         treads 2
-        request-stackoverflow (request-stackoverflow tags treads)
+        reqest (when-not (nil? tags) (request-stackoverflow tags treads))
         formated-output (cheshire/generate-string
-                         (reduce into {} request-stackoverflow)
+                         reqest
                          {:pretty true})]
-    {:status 200
-     :body (if (nil? tags)
-             "Не верный запрос"
-             formated-output)
-     :headers {}}))
+    (if (nil? tags)
+      {:status 400
+       :body "Не верный запрос"
+       :headers {}}
+      {:status 200
+       :body formated-output
+       :headers {}})))
 
 
 (defn start-server []
@@ -93,7 +93,6 @@
   (start-server))
 
 
-(comment 
-  (start-server)
+(comment
+  (start-server) 
   )
-  
